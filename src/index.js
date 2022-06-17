8eb9116c6b60f31ef90d34cd61865946224dfacc
Maryna Markova <markova.maryna@gmail.com>
2022-06-17 12:40:29 +0300

    finish all fake data to five cities

--- src/index.js
+++ src/index.js
@@ -22,9 +22,9 @@ let weather = {
 };
 
 let city = prompt("Enter a city");
-city = city.toLocaleLowerCase().trim();
+city = city.toLowerCase().trim();
 
-if (city === "paris")
+if (city === "paris") {
   alert(
     `It is currently ${Math.round(weather.paris.temp)}°C (${Math.round(
       weather.paris.temp * 1.8 + 32
@@ -32,7 +32,42 @@ if (city === "paris")
       city.charAt(0).toUpperCase() + city.slice(1)
     } with a humidity of ${weather.paris.humidity}%`
   );
-
-alert(
-  `Sorry, we don't know the weather for this city, try going to https://www.google.com/search?q=weather+sydney`
-);
+} else if (city === "tokyo" || city === "tokio") {
+  alert(
+    `It is currently ${Math.round(weather.tokyo.temp)}°C (${Math.round(
+      weather.tokyo.temp * 1.8 + 32
+    )}°F) in ${
+      city.charAt(0).toUpperCase() + city.slice(1)
+    } with a humidity of ${weather.tokyo.humidity}%`
+  );
+} else if (city === "lisbon") {
+  alert(
+    `It is currently ${Math.round(weather.lisbon.temp)}°C (${Math.round(
+      weather.lisbon.temp * 1.8 + 32
+    )}°F) in ${
+      city.charAt(0).toUpperCase() + city.slice(1)
+    } with a humidity of ${weather.lisbon.humidity}%`
+  );
+} else if (city === "san francisco" || city === "san-francisco") {
+  alert(
+    `It is currently ${Math.round(
+      weather["san francisco"]["temp"]
+    )}°C (${Math.round(
+      weather["san francisco"]["temp"] * 1.8 + 32
+    )}°F) in San Francisco with a humidity of ${
+      weather["san francisco"]["humidity"]
+    }%`
+  );
+} else if (city === "oslo") {
+  alert(
+    `It is currently ${Math.round(weather.oslo.temp)}°C (${Math.round(
+      weather.oslo.temp * 1.8 + 32
+    )}°F) in ${
+      city.charAt(0).toUpperCase() + city.slice(1)
+    } with a humidity of ${weather.oslo.humidity}%`
+  );
+} else {
+  alert(
+    `Sorry, we don't know the weather for this city, try going to https://www.google.com/search?q=weather+${city}`
+  );
+}
