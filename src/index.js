4d409a26ede6b514fb89c3aaee40c8fdba5afbe3
Maryna Markova <markova.maryna@gmail.com>
2022-07-01 15:25:25 +0300

    mute current button and fix bugs

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+// set time and date
+
 let now = new Date();
 
 let dayPlace = document.querySelector("#day");
@@ -74,6 +76,15 @@ celcius.addEventListener("click", celciusTemp);
 
 // City search
 
+function displayWeather(response) {
+  let weatherTemperature = document.querySelector("#tremperature-big");
+  let temperature = Math.round(response.data.main.temp);
+  //let description = response.data.weather[0].description;
+
+  console.log(temperature);
+  weatherTemperature.innerHTML = temperature;
+}
+
 function search(event) {
   event.preventDefault();
 
@@ -89,20 +100,46 @@ function search(event) {
     city = `Truskavets`;
     alert(`Please type a city`);
   }
+
+  // Get current temperature
+
   let apiKey = `09de8678225621c95e40390774879e02`;
 
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(displayWeather);
-
-  function displayWeather(response) {
-    let weatherTemperature = document.querySelector("#tremperature-big");
-    let temperature = Math.round(response.data.main.temp);
-    //let description = response.data.weather[0].description;
-
-    console.log(temperature);
-    weatherTemperature.innerHTML = temperature;
-  }
 }
 let form = document.querySelector(`#search-form`);
 form.addEventListener("submit", search);
+
+//   // Current Location Button
+
+//   function displayWeather(response) {
+//     let weatherTemperature = document.querySelector("#tremperature-big");
+//     let temperature = Math.round(response.data.main.temp);
+//     //let description = response.data.weather[0].description;
+
+//     console.log(temperature);
+//     weatherTemperature.innerHTML = temperature;
+//   }
+
+//   function getCurrentPosition() {
+//     navigator.geolocation.getCurrentPosition(showPosition);
+//   }
+
+//   function showPosition(position) {
+//     let h1 = document.querySelector("h1");
+//     let lat = position.coords.latitude;
+//     let lon = position.coords.longitude;
+
+//     h1.innerHTML = `Your Latitude is ${lat} and your longitude is ${lon}`;
+
+//     let apiKey = `09de8678225621c95e40390774879e02`;
+
+//     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=lat={lat}&lon={lon}&appid=${apiKey}&units=metric`;
+
+//     axios.get(apiUrl).then(displayWeather);
+//   }
+
+// let currentButton = document.querySelector("#currentLocation");
+// currentButton.addEventListener("click", getCurrentPosition);
